2d80170a3b25209bb07c340dc0187228188c532c
vngo00 <phucvinh1131@gmail.com>
2023-04-08 16:17:13 -0700

    successfully render images from api to index page

--- application/public/js/index.js
+++ application/public/js/index.js
@@ -1,4 +1,4 @@
-var url = 'https://dummyjson.com/products'
+var url = 'https://jsonplaceholder.typicode.com/albums/2/photos'
 
 
 
@@ -13,63 +13,57 @@ var url = 'https://dummyjson.com/products'
             </div>
         </div>`
  */
+        function fadeOut(ev){
+            var ele = ev.currentTarget;
+        
+            ele.style.transition = 'opacity .5s';
+            ele.style.opacity = 0;
+            let timer = setInterval(function () {
+                ele.remove();
+                clearInterval(timer);
+            }, 500);
+            //console.log(ev.currentTarget);
+        
+        }
 
-function makeElement(type, attr, text, children){
-
+function makeElement(type, attr, src, children){
+    var e = document.createElement(type);
+    if(attr){
+        e.setAttribute('class', attr);
+    }
+    if (src){
+        e.setAttribute('src', src);
+    }
+    if (children){
+        children.forEach(function (child) {
+            e.appendChild(child);
+        })
+    }
+    return e;
 }
 
 
-function fadeOut(ev){
-    var ele = ev.currentTarget;
-
-    ele.style.transition = 'opacity .5s';
-    ele.style.opacity = 0;
-    let timer = setInterval(function () {
-        ele.remove();
-        clearInterval(timer);
-    }, 400);
-    console.log(ev.currentTarget);
-
-}
 function buildCard(data){
-    var cardDiv = document.createElement("div"); //, <div></div>
-    cardDiv.setAttribute('class', 'product-card');
-
-    var imgTag = document.createElement("img");
-    imgTag.setAttribute("class", "product-img");
-    imgTag.setAttribute("src", data.thumbnail);
-
-    var titleTag = document.createElement("p");
-    titleTag.setAttribute("class", "product-title");
-    titleTag.appendChild(document.createTextNode(data.title));
-
-
-    var costTag = document.createElement("p");
-    costTag.setAttribute("class", "product-cost");
-    costTag.appendChild(document.createTextNode(data.price));
-
-    var productDiv = document.createElement("div"); //, <div></div>
-    productDiv.setAttribute('class', 'product-info');
-
-    productDiv.appendChild(titleTag);
-    productDiv.appendChild(costTag);
-
-    cardDiv.appendChild(imgTag);
-    cardDiv.appendChild(productDiv);
-
+    var imgTag = makeElement('img', 'product-img', data.thumbnailUrl, null);
+    var titleTag = makeElement('p', 'product-title', null, [document.createTextNode(data.title)]);
+    var productDiv = makeElement('div', 'product-info', null, [titleTag])
+    var cardDiv = makeElement('div', 'product-card', null, [imgTag, productDiv]);
     cardDiv.addEventListener('click', fadeOut);
     return cardDiv;
-    
-    
-    
 }
+
+
+
+
+/**
+ * fetching products from PIT and render it onto the index page
+ */
 
 async function fetchWithDOMAPI(){
     try{
         var response = await fetch(url);
         var data = await response.json();
-        var elements = data.products.map(buildCard);
-        //console.log(elements);
+        var elements = data.map(buildCard);
         document.getElementById('product-list').append(...elements);
     }
     catch(err){
