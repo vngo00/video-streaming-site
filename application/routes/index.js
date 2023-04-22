e74a9837113af4b80ceb3d20907845243e8ef58b
vngo00 <phucvinh1131@gmail.com>
2023-04-22 14:20:03 -0700

    update layout for adding correct links to correct end points

--- application/routes/index.js
+++ application/routes/index.js
@@ -4,24 +4,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+  res.render('index', { title: 'Upipe', name:"[Insert your name here]" , js:["index.js"]});
 });
 
 router.get('/login', function(req, res) {
-  res.render('login');
+  res.render('login', {title:'login'});
 });
 
 router.get('/profile', function(req, res){
-  res.render('profile');
+  res.render('profile', {title:'profile'});
 });
 router.get('/registration', function(req, res){
-  res.render('registration');
+  res.render('registration', {title:'registration', js:["formValidation.js"]});
 });
 router.get('/viewpost', function(req, res){
-  res.render('viewpost');
+  res.render('viewpost', {title:'viewpost'});
 });
 router.get('/postvideo', function(req, res){
-  res.render('postvideo');
+  res.render('postvideo', {title:'postvideo'});
 });
 
 
