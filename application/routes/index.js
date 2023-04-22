4fff3c98d241c8bb25fac477fe34a2fa0fb2106b
vngo00 <phucvinh1131@gmail.com>
2023-04-22 13:37:31 -0700

    update all html files to hbs files and sucessfully render them

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
 });
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.get('/profile', function(req, res){
+  res.render('profile');
+});
+router.get('/registration', function(req, res){
+  res.render('registration');
+});
+router.get('/viewpost', function(req, res){
+  res.render('viewpost');
+});
+router.get('/postvideo', function(req, res){
+  res.render('postvideo');
+});
+
 
 module.exports = router;
